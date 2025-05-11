63a25c3e34998eb67d0e4815c5866192fa2549d9
Марат Кабардоков <kabardokov.m@yandex.ru>
2025-05-11 22:26:30 +0300

    Улучшил админку еще

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
 
-from .models import Category, Wrapper, Topping, IceCream
+from .models import Category, IceCream, Topping, Wrapper
 
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+
+
+admin.site.register(IceCream, IceCreamAdmin)
 admin.site.register(Category)
 admin.site.register(Wrapper)
 admin.site.register(Topping)
-admin.site.register(IceCream)
+
